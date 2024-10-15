e748e59c04e2e04f8c825f6c8722f11bede47e4a
geo-tat <nosleep2night@yandex.ru>
2024-10-15 19:15:32 +0400

    changed Hashmap to array

--- src/util/CustomHashSet.java
+++ src/util/CustomHashSet.java
@@ -1,33 +1,116 @@
 package util;
 
-import java.util.HashMap;
-import java.util.Set;
-
 public class CustomHashSet<T> {
-    private final HashMap<T, Boolean> map;
-    private static final Boolean PRESENT = Boolean.TRUE;
+    private int capacity = 10;
+    private int size = 0;
+    private final double LOAD_COEFFICIENT = 0.8;
+    private Object[] array;
 
     public CustomHashSet() {
-        map = new HashMap<>();
+        array = new Object[capacity];
     }
 
-    public void put(T key) {
-        map.put(key, PRESENT);
-    }
-
-    public boolean contains(T key) {
-        return map.containsKey(key);
-    }
-
-    public void delete(T key) {
-        map.remove(key);
-    }
-
-    public Set<T> iterate() {
-        return map.keySet();
+    private void resize() {
+        capacity = array.length * 2;
+        Object[] newArray = new Object[capacity];
+        for (Object element : array) {
+            if (element != null) {
+                int newHash = Math.abs(element.hashCode() % capacity);
+                newArray[newHash] = element;
+            }
+        }
+        array = newArray;
     }
 
     public int size() {
-        return map.size();
+        return size;
+    }
+
+    public boolean put(T object) {
+        if (object == null) {
+            if (contains(null)) {
+                return false;
+            }
+        } else {
+
+            if (size >= capacity * LOAD_COEFFICIENT) {
+                resize();
+            }
+        }
+        int hash = getHashIndex(object);
+        if (array[hash] == null) {
+            array[hash] = object;
+            size++;
+            return true;
+        } else if (array[hash].equals(object)) {
+            return false;
+        } else {
+
+            int originalHash = hash;
+            while (array[hash] != null) {
+                if (array[hash].equals(object)) {
+                    return false;
+                }
+                hash = (hash + 1) % capacity;
+                if (hash == originalHash) {
+                    return false;
+                }
+            }
+            array[hash] = object;
+            size++;
+            return true;
+        }
+    }
+
+    private int getHashIndex(T object) {
+        return Math.abs(object.hashCode() % capacity);
+    }
+
+    public boolean contains(T object) {
+        int hash = getHashIndex(object);
+        int originalHash = hash;
+        while (array[hash] != null) {
+            if (array[hash].equals(object)) {
+                return true;
+            }
+            hash = (hash + 1) % capacity;
+            if (hash == originalHash) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public boolean delete(T object) {
+        if (object == null) {
+            return false;
+        }
+        int hash = getHashIndex(object);
+        int tempHash = hash;
+        while (array[hash] != null) {
+            if (array[hash].equals(object)) {
+                array[hash] = null;
+                size--;
+
+                hash = (hash + 1) % capacity;
+                while (array[hash] != null) {
+                    T rehashObject = (T) array[hash];
+                    array[hash] = null;
+                    size--;
+                    put(rehashObject);
+                    hash = (hash + 1) % capacity;
+                }
+                return true;
+            }
+            hash = (hash + 1) % capacity;
+            if (hash == tempHash) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public T[] iterate() {
+        return (T[]) array;
     }
 }
