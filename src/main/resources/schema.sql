94ea38a3a1d06b3ffa14a8f778c176a3b93edab2
geo-tat <nosleep2night@yandex.ru>
2024-10-23 11:08:08 +0400

    changed serial to bigint in schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,13 +2,13 @@
 CREATE TYPE ticket_type AS ENUM ('DAY', 'WEEK', 'MONTH', 'YEAR');
 
 CREATE TABLE user_info (
-    id SERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE ticket (
-    id SERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     user_id INT REFERENCES user_info(id) ON DELETE CASCADE,
     ticket_type ticket_type NOT NULL,
     creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
